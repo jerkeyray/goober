126b80c106e7c122cb8dd9fc4bde73feb4d03dc0
srivastavya02 <srivastavya24@gmail.com>
2025-08-06 19:26:10 +0530

    idk

--- tools.go
+++
@@ -1,8 +0,0 @@
-//go:build tools
-// +build tools
-
-// This file is used to track tool dependencies
-package tools
-
-// Tool dependencies for development
-// Note: cmd/goober is not importable as a package, so we don't import it here
